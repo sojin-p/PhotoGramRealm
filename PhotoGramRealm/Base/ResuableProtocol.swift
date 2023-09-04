b8922f9f7596102a4b6f3be1a529972f458035bb
Jack <devjack78@gmail.com>
2023-09-05 08:44:30 +0900

    Initial Commit

---
+++ PhotoGramRealm/Base/ResuableProtocol.swift
@@ -0,0 +1,30 @@
+//
+//  ResuableProtocol.swift
+//  PhotoGramRealm
+//
+//  Created by jack on 2023/09/03.
+//
+
+import UIKit
+
+protocol ReusableViewProtocol: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+extension UIViewController: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+extension UITableViewCell: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
